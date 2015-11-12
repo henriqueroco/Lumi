8f04121ddcc2810f471437843fe0ef5087bc0ed6
Henrique <henrique.roco@gmail.comk>
2015-11-11 22:03:55 -0200

    Limpeza projeto

--- Lumi/src/main/webapp/scripts/services/AgendaFactory.js
+++
@@ -1,4 +0,0 @@
-angular.module('lumi').factory('AgendaResource', function($resource){
-    var resource = $resource('rest/agendas/:AgendaId',{AgendaId:'@id'},{'queryAll':{method:'GET',isArray:true},'query':{method:'GET',isArray:false},'update':{method:'PUT'}});
-    return resource;
-});
